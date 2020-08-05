bdb4c9e807ac2ad97c692cf91bc12fd971f7a127
abinaya910 <68158322+abinaya910@users.noreply.github.com>
2020-08-05 19:32:32 +0530

    Create text.py

---
+++ text.py
@@ -0,0 +1,85 @@
+import pandas as pd
+import numpy as np
+import sys
+import nltk
+nltk.download('stopwords')
+from nltk.tokenize import RegexpTokenizer
+from nltk.corpus import stopwords
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, LSTM
+from keras.utils import np_utils
+from keras.callbacks import ModelCheckpoint
+file=open('/content/book.txt').read()
+def tokenize_words(input):
+  input=input.lower()
+  tokenizer=RegexpTokenizer(r'\w+')
+  tokens=tokenizer.tokenize(input)
+  filtered=filter(lambda token: token not in stopwords.words('english'),tokens)
+  return"".join(filtered)
+
+processed_inputs=tokenize_words(file)
+
+#chars to numbers
+chars=sorted(list(set(processed_inputs)))
+char_to_num=dict((c,i) for i, c in enumerate(chars))
+
+input_len=len(processed_inputs)
+vocab_len=len(chars)
+print("Total number of characters:", input_len)
+print("Total number of vocab:", vocab_len)
+
+seq_length=100
+x_data=[]
+y_data=[]
+
+for i in range(0,input_len-seq_length,1):
+  in_seq=processed_inputs[i:i + seq_length]
+  out_seq=processed_inputs[i + seq_length]
+  x_data.append([char_to_num[char] for char in in_seq])
+  y_data.append(char_to_num[out_seq])
+
+n_patterns= len(x_data)
+print("Total patterns:", n_patterns)
+
+
+X=np.reshape(x_data,(n_patterns,seq_length, 1))
+X=X/float(vocab_len)
+
+model=Sequential()
+model.add(LSTM(256, input_shape=(X.shape[1],X.shape[2]), return_sequences=True))
+model.add(Dropout(0.2))
+model.add(LSTM(256, return_sequences=True))
+model.add(Dropout(0.2))
+model.add(LSTM(128))
+model.add(Dropout(0.2))
+model.add(Dense(y.shape[1],activation='softmax'))
+
+model.compile(loss='categorical_crossentropy', optimizer='adam')
+
+filepath="model_weights_saved.hdf5"
+checkpoint=ModelCheckpoint(filepath, monitor='loss', verbose=1, save_best_only=True, mode='min')
+desired_callbacks=[checkpoint]
+
+model.fit(X,y, epochs=4, batch_size=256, callbacks=desired_callbacks)
+
+filename="model_weights_saved.hdf5"
+model.load_weights(filename)
+model.compile(loss='categorical_crossentropy',optimizer='adam')
+
+num_to_char=dict((i,c) for i, c in enumerate(chars))
+
+start=np.random.randint(0, len(x_data)=1)
+patterns=x_data[start]
+print("Random Seeds:")
+print("\"",''.join([num_to_char[value]for  value in patterns])."\"")
+
+for i in range(1000):
+  x=np.reshape(patterns,[1, len(patterns), 1])
+  x=xfloat(vocab_len)
+  pred=model.predict(x, verbose=3)
+  index=np.argmax[pred]
+  result=num_to_char[index]
+  seg_in=[num_to_char[value] for value in patterns]
+  sys.stdout.write(result)
+  pattern.append(index)
+  pattern=pattern[1: len(pattern)]
